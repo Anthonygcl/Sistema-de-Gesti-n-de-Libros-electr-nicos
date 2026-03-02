c0dbd737a7d8d62e972fc38444b136be5bdf341c
Anthonygcl <anthonypaloyoya@gmail.com>
2026-03-01 21:43:24 -0500

    Update libro.go

--- libro.go
+++ libro.go
@@ -5,35 +5,22 @@ import (
 	"fmt"
 )
 
-// Libro con campos privados (Encapsulación)
 type Libro struct {
-	id      int
-	titulo  string
-	autor   string
-	paginas int
-	formato string
+	ID      int    `json:"id"`
+	Titulo  string `json:"titulo"`
+	Autor   string `json:"autor"`
+	Paginas int    `json:"paginas"`
+	Formato string `json:"formato"`
 }
 
-// NuevoLibro es el constructor con manejo de errores
 func NuevoLibro(id int, titulo string, autor string, paginas int, formato string) (*Libro, error) {
 	if paginas <= 0 {
-		return nil, errors.New("el libro debe tener al menos 1 página")
+		return nil, errors.New("las páginas deben ser un número positivo")
 	}
-	if titulo == "" {
-		return nil, errors.New("el título no puede estar vacío")
-	}
-	return &Libro{id, titulo, autor, paginas, formato}, nil
+	return &Libro{ID: id, Titulo: titulo, Autor: autor, Paginas: paginas, Formato: formato}, nil
 }
 
-// MÉTODOS GETTERS (Para que otros paquetes puedan leer los datos)
-func (l *Libro) Titulo() string  { return l.titulo }
-func (l *Libro) Autor() string   { return l.autor }
-func (l *Libro) Paginas() int    { return l.paginas }
-func (l *Libro) Formato() string { return l.formato }
-
-// ObtenerDetalles implementa la interfaz Gestionable
 func (l *Libro) ObtenerDetalles() string {
-	return fmt.Sprintf("[%s] %s - %d pág. (%s)", l.autor, l.titulo, l.paginas, l.formato)
+	return fmt.Sprintf("[%s] %s - %d pág.", l.Autor, l.Titulo, l.Paginas)
 }
-
 
