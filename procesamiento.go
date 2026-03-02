8742fb6ac70eb7f935bb3b2d89a2b63f2d8ebb35
Anthonygcl <anthonypaloyoya@gmail.com>
2026-03-01 21:54:48 -0500

    Update procesamiento.go

--- procesamiento.go
+++ procesamiento.go
@@ -7,28 +7,15 @@ import (
 
 type PredicadoLibro func(models.Libro) bool
 
-func FiltrarLibros(libros []models.Libro, criterio PredicadoLibro) []models.Libro {
-	var resultado []models.Libro
+func Filtrar(libros []models.Libro, p PredicadoLibro) []models.Libro {
+	var res []models.Libro
 	for _, l := range libros {
-		if criterio(l) {
-			resultado = append(resultado, l)
-		}
+		if p(l) { res = append(res, l) }
 	}
-	return resultado
+	return res
 }
 
-// CORRECCIÓN: Usamos l.Formato() en lugar de l.Formato
-func CriterioPorFormato(formato string) PredicadoLibro {
-	return func(l models.Libro) bool {
-		return strings.EqualFold(l.Formato(), formato)
-	}
-}
+func EsPDF(l models.Libro) bool { return strings.EqualFold(l.Formato, "PDF") }
 
-// CORRECCIÓN: Usamos l.Paginas() en lugar de l.Paginas
-func CriterioLibrosLargos() PredicadoLibro {
-	return func(l models.Libro) bool {
-		return l.Paginas() > 400
-	}
-}
 
 
